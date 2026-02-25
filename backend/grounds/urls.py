fd259253e477b8989a7cb4b6c6c5987680e64c88
Mahika Lakoul <mahikalakoul01@gmail.com>
2026-02-25 16:07:08 +0545

    url of ground availability

--- backend/grounds/urls.py
+++ backend/grounds/urls.py
@@ -1,8 +1,14 @@
-# grounds/urls.py
+from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import GroundViewSet
+from .views import GroundViewSet, GroundAvailabilityBulkUpsertView
 
 router = DefaultRouter()
 router.register(r"grounds", GroundViewSet, basename="grounds")
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    path(
+        "grounds/<int:pk>/availability/bulk/",
+        GroundAvailabilityBulkUpsertView.as_view(),
+        name="ground-availability-bulk",
+    ),
+]
