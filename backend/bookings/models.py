13ce02b804b38f827ff389128fbecca2f16da652
Mahika Lakoul <mahikalakoul01@gmail.com>
2026-03-10 15:08:38 +0545

    new class for public booking

--- backend/bookings/models.py
+++ backend/bookings/models.py
@@ -1,3 +1,4 @@
+# bookings/models.py
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
@@ -14,11 +15,16 @@ class Booking(models.Model):
         ONLINE = "ONLINE", "Online"
         OFFLINE = "OFFLINE", "Offline/Physical"
 
+    class BookingType(models.TextChoices):
+        OPEN = "OPEN", "Open Booking"
+        CLOSED = "CLOSED", "Closed Booking"
+
     ground = models.ForeignKey(
         Ground,
         on_delete=models.CASCADE,
         related_name="bookings"
     )
+
     date = models.DateField()
     start_time = models.TimeField()
     end_time = models.TimeField()
@@ -50,6 +56,21 @@ class Booking(models.Model):
         choices=Status.choices,
         default=Status.PENDING
     )
+
+    booking_type = models.CharField(
+        max_length=15,
+        choices=BookingType.choices,
+        default=BookingType.CLOSED
+    )
+
+    # total players currently in open booking
+    current_players = models.PositiveIntegerField(default=1)
+
+    # how many players needed in total
+    required_players = models.PositiveIntegerField(default=1)
+
+    # optional note for open game
+    open_game_note = models.TextField(blank=True, default="")
 
     # eSewa fields
     transaction_uuid = models.CharField(
@@ -83,3 +104,15 @@ class Booking(models.Model):
 
     def __str__(self):
         return f"{self.ground_id} {self.date} {self.start_time}-{self.end_time}"
+
+    @property
+    def spots_left(self):
+        return max(self.required_players - self.current_players, 0)
+
+    @property
+    def is_open_joinable(self):
+        return (
+            self.booking_type == self.BookingType.OPEN
+            and self.status == self.Status.BOOKED
+            and self.current_players < self.required_players
+        )
