90037276db9ba3469a004b0ee72b813c8339282a
Mahika Lakoul <mahikalakoul01@gmail.com>
2026-04-02 22:17:18 +0545

    add path for player detail, player list,  profile view

--- backend/authapp/urls.py
+++ backend/authapp/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
-from .views import RegisterView, LoginView, ProfileView
+from .views import (
+    LoginView,
+    PlayerDetailView,
+    PlayerListView,
+    ProfileView,
+    RegisterView,
+)
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
     path("profile/", ProfileView.as_view(), name="profile"),
+
+    path("players/", PlayerListView.as_view(), name="players-list"),
+    path("players/<int:user_id>/", PlayerDetailView.as_view(), name="player-detail"),
 ]
